e1b2aeb827bbd91f12bcc2964a335b197c7e83ba
Khang <tuongkhang9a4@gmail.com>
2021-07-10 20:03:16 +0700

    init
    create UI

---
+++ Common_UI (Updated)/App.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+
+//HOMEPAGE.
+const path = require('path');
+app.set('view engine', 'hbs');
+const publicDirectory = path.join(__dirname, './public');
+console.log(__dirname);
+app.use(express.static(publicDirectory));
+
+// 2 hàm này dùng để nhận .json mỗi khi nhấn submit, ... từ đó ms có đc thông tin mà người dùng nhập.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Nếu không sử dụng routes.
+// app.get('/', (req, res) => {
+//     //res.send('<h1>HomePage</h1>')
+//     res.render('index')
+// });
+
+// app.get('/login', (req, res) => {
+//     //res.send('<h1>HomePage</h1>')
+//     res.render('login')
+// });
+
+// Nếu sử dụng routes. "ký hiệu ./ là cùng thu mục với file đang code."
+// //Define Routes.
+app.use('/', require('./routes/pages'));
+app.use('/auth/', require('./routes/auth'));
+
+
+
+// dùng để tạo port
+app.listen(10001, () => {
+    console.log('Server started on port 10001');
+})
+
+
+
+//DOTENV. //using .env file protect information in the database of mysql.
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
+//MYSQL :  kết nối với mysql từ phpAdmin.
+const mysql = require('mysql');
+const database = mysql.createConnection({
+    //Link MYSQL khi không dùng .env
+   // host: 'localhost', // sử dụng trên máy tính của mình --> Localhost.
+   // user: 'root', // do sử  dụng XAMPP nên user mặc định là host, password trống không.
+   // password: '',
+   // database: 'MoodleMini' // tên database mà mình đặt trong mysql của phpAdmin.
+    
+    //Link MySQL khi sử dụng .env, lúc này thay vì để lộ thông tin thì ta dùng biến khác để che đi, thông tin chi tiết nằm ở file .env
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DATABASE_PORT
+});
+
+//kết nới với sql.
+database.connect((error) => {
+    if (error) {
+        console.log(error);
+    } else { // nếu kết nối thành công thì ở terminal sẽ in ra MySQL connected.
+        console.log('MySQL connected.');
+    }
+});
+
+// Buộc phải có hàm này, bởi vì khi kết nới vs sql ở trên internet sẽ có khoảng thời gian timeout
+// Nếu hết thời gian timeout đó (ý là trong quá trình đó ko sử dụng truy vấn) ==> server sẽ đóng ==> ném ra lỗi error.
+// Để server ko đóng ==> dùng hàm này để liên tục truy vấn ==> Chương trình chạy bth.
+setInterval(function () {
+    database.query('SELECT 1');
+}, 10001);
---
+++ Common_UI (Updated)/routes/pages.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// hiện tại / là đang ở thư mục views. --> muốn dẫn tới lecturer thì /login_actors/login_lecturer
+router.get('/login_actors/login_lecturer', (req, res) => {
+    res.render('login_actors/login_lecturer');
+});
+
+router.get('/login_actors/login_student', (req, res) => {
+    res.render('login_actors/login_student');
+});
+
+router.get('/login_actors/login_staff', (req, res) => {
+    res.render('login_actors/login_staff');
+});
+
+
+// ĐĂNG NHẬP THÀNH CÔNG ==> HIỆN GIAO DIỆN ỨNG VỚI TỪNG ACTORS.
+//Giao diện của Student.
+router.get('/student/student_UI', (req, res) => {
+    res.render('student/student_UI');
+});
+
+
+//Giao diện của Lecturer.
+router.get('/lecturer/lecturer_UI', (req, res) => {
+    res.render('lecturer/lecturer_UI');
+});
+
+//Giao diện của Staff.
+router.get('/staff/staff_UI', (req, res) => {
+    res.render('staff/staff_UI');
+});
+
+
+module.exports = router;
